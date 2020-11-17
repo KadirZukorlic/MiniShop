7d2dde6459526585a9aac9adea8d10e5e04a195f
Kadir Zurkolic <kadirzukorlic@gmail.com>
2020-11-17 22:29:44 +0100

    Objects changed to classes, buttons enabled

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,42 +1,124 @@
-const productList = {
-  products: [
-    {
-      title: 'A pillow',
-      imageUrl:
-        'https://www.mattressclarity.com/wp-content/uploads/2017/11/Brooklinen-mid-plush-down-pillow-review-side.jpg',
-      price: 19.99,
-      description: 'A soft pillow!',
-    },
-    {
-      title: 'A carpet',
-      imageUrl:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkNrInfTR2UwrPvYTUmOl0-bbarGsyT5ZHrg&usqp=CAU',
-      price: 89.99,
-      description: 'A carpet which you might like - or not.',
-    },
-  ],
+class Product {
+  //   title = 'DEFAULT';           //class field
+  // imageUrl;
+  //   price;
+  //   description;
+
+  constructor(title, image, desc, price) {
+    this.title = title; //class property
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+class ShoppingCart {
+  items = [];
+
+  addProduct(product) {
+    this.items.push(product);
+    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+
+  }
+
   render() {
-    const renderHook = document.getElementById('app');
+    const cartEl = document.createElement('section');
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    cartEl.className = 'cart';
+    this.totalOutput = cartEl.querySelector('h2');
+    return cartEl;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>/$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+      `;
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      'A pillow',
+      'https://www.mattressclarity.com/wp-content/uploads/2017/11/Brooklinen-mid-plush-down-pillow-review-side.jpg',
+      'A soft pillow!',
+      19.99
+    ),
+    new Product(
+      'A Carpet',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkNrInfTR2UwrPvYTUmOl0-bbarGsyT5ZHrg&usqp=CAU',
+      'A carpet which you might like - or not.',
+      89.99
+    ),
+  ];
+
+  constructor() {}
+
+  render() {
     const prodList = document.createElement('ul');
     prodList.className = 'product-list';
     for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-          <div>
-            <img src="${prod.imageUrl}" alt="${prod.title}" >
-            <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>/$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-          </div>
-          `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-    renderHook.append(prodList);
-  },
-};
+    return prodList;
+  }
+}
 
-productList.render();
+class Shop {
+  render() {
+    const renderHook = document.getElementById('app');
+
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
+}
+
+class App {
+static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product)
+  }
+}
+
+App.init();
+
